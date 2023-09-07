740dc7c487d94a4fd3247d59c22782ab3fd46115
lsakee <93514333+lsakee@users.noreply.github.com>
2023-09-08 00:22:13 +0900

    [ADD] : Di Module #1

---
+++ feature-home/src/main/java/com/lsakee/feature_home/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.lsakee.feature_home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
